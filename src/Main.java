ea02f31b995f4f4d37dd620dcdf5ccc02b29c64e
Tewabe Tadege <ethiohabesha412@gmail.com>
2022-08-26 03:20:55 -0700

    Data Types

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+class Main {
+    public static void main(String[] args) {
+// primitives variables
+        byte by = 127;
+        System.out.println(by);
+        byte low = -128;
+        System.out.println(low);
+
+        short s = 32_767;
+        System.out.println(s);
+        short s2 = -32_768;
+        System.out.println(s2);
+
+        int i = 2_147_483_647;
+        System.out.println(i);
+        int i2 = -2_147_483_648;
+        System.out.println(i2);
+
+        long l = 9_223_372_036_854_775_807L;
+        System.out.println(l);
+
+        double d = 3839.34839;
+        System.out.println(d);
+
+        float f = 3839.34839F;
+        System.out.println(f);
+        float x2 = 3.14F;
+        System.out.println(x2);
+
+        boolean on = false;
+        System.out.println(on);
+        boolean off = true;
+        System.out.println(off);
+
+        char c = 'a';
+        System.out.println(c);
+        char c2 = 'z';
+        System.out.println(c2);
+//variable is a reference variable
+        String sentence = "the quick brown fox jumps over the lazy dog";
+        System.out.println(sentence);
+        String mastere = "I mastered Java data types!";
+        System.out.println(mastere);
+
+    } // End of Main method
+} // End of Main class
